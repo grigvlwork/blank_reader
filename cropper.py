2e7d81e99352ecfd1bd2d7525d7acf3ae671c882
grigorovich <grigvlwork@gmail.com>
2023-12-19 12:37:27 +0700

    добавил кнопку, начал делать обработку файлов

--- cropper.py
+++ cropper.py
@@ -1,15 +1,21 @@
+import os
+from os import listdir
+from os.path import isfile, join
 import sys
 import qdarkstyle
 import traceback
+import os
 
 from PyQt5 import uic, QtCore, QtGui
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox, QMenu, QGraphicsPixmapItem, \
     QGraphicsItem
 from PyQt5.QtGui import QStandardItem, QStandardItemModel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
 from PyQt5.Qt import QClipboard
 from PyQt5.QtCore import QModelIndex
 import icons_rc
 from PyQt5.QtGui import QPixmap
+from PIL import Image
 
 from cropper_ui import Ui_MainWindow
 
@@ -19,8 +25,12 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.theme = 'Dark'
+        self.work_dir = None
+        self.source_dir = None
+        self.files = []
+        self.thumbnails = []
         self.theme_btn.clicked.connect(self.change_theme)
-        self.open_btn.clicked.connect(self.open_image)
+        self.open_btn.clicked.connect(self.open_folder)
         self.source_lb.setText('')
         self.result_lb.setText('')
 
@@ -36,6 +46,30 @@ class MyWidget(QMainWindow, Ui_MainWindow):
             self.theme_btn.setIcon(icon)
             app.setStyleSheet(qdarkstyle.load_stylesheet(qt_api='pyqt5', palette=qdarkstyle.DarkPalette))
 
+    def open_folder(self):
+        self.source_dir = QFileDialog.getExistingDirectory(self, 'Select Folder')
+        self.files = [os.path.join(self.source_dir, f) for f in os.listdir(self.source_dir) if
+                     os.path.isfile(os.path.join(self.source_dir, f))]
+        if os.path.isdir(self.source_dir + '/cropper'):
+            self.work_dir = self.source_dir + '/cropper'
+        else:
+            os.mkdir(self.source_dir + '/cropper')
+            self.work_dir = self.source_dir + '/cropper'
+            os.mkdir(self.work_dir + '/data')
+            os.mkdir(self.work_dir + '/thumbnails')
+            os.mkdir(self.work_dir + '/output')
+            if len(self.files) > 0:
+                self.generate_thumbnails()
+
+    def generate_thumbnails(self):
+        for file in self.files:
+            image = Image.open(file)
+            image.thumbnail((300, 300))
+            new_name = self.work_dir + '/thumbnails/' + os.path.basename(file)
+            image.save(new_name)
+            self.thumbnails.append(new_name)
+
+
     def open_image(self):
         # https://ru.stackoverflow.com/questions/1263508/Как-добавить-изображение-на-qgraphicsview
         fname, _ = QFileDialog.getOpenFileName(
@@ -44,8 +78,7 @@ class MyWidget(QMainWindow, Ui_MainWindow):
             pic = QGraphicsPixmapItem()
             pic.setPixmap(QPixmap(fname).scaled(160, 160))
             pic.setFlags(QGraphicsItem.ItemIsSelectable | QGraphicsItem.ItemIsMovable)
-            pic.setOffset(12, 12)
-            self.source_gv.scene.addItem(pic)
+
 
 
 def excepthook(exc_type, exc_value, exc_tb):
